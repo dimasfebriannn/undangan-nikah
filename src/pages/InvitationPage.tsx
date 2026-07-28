f994932b4657c0846d037d7031fb9b6f7955b1ff
fbrn224 <eldimsvr11@gmail.com>
2026-07-28 20:21:47 +0700

    feat: increase music volume to 70%

--- src/pages/InvitationPage.tsx
+++ src/pages/InvitationPage.tsx
@@ -36,7 +36,7 @@ export function InvitationPage() {
     audioRef.current = audio
     audio.play().then(() => {
       let vol = 0
-      const target = 0.5
+      const target = 0.7
       const step = target / 30
       const id = setInterval(() => {
         vol += step
